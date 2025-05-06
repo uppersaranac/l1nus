8597458b875a1070a6fe6eb27fe8c3325093ade8
Lewis Geer <lewis.geer@nist.gov>
2025-05-06 16:02:04 -0500

    fix bugs when running in mpi mode

--- llm/run_mpi.sh
+++ llm/run_mpi.sh
@@ -6,7 +6,6 @@
 #SBATCH --mail-user=lewis.geer@gmail.com
 #SBATCH --mail-type=all
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=32
 #SBATCH -o logs/%x_%j.out
 #SBATCH -e logs/%x_%j.err
 # ----------------------------------------------------------
@@ -19,31 +18,69 @@ source ~/source/l1nus/llm/.venv/bin/activate
 
 # outside of script run `accelerate config` to set up 
 
-export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
-export NCCL_DEBUG=INFO
-export NCCL_SOCKET_IFNAME=ib0
+# export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+# export NCCL_DEBUG=INFO
+# export NCCL_SOCKET_IFNAME=ib0
 
+# see https://github.com/shaneholloman/smollm/blob/542250b39015654d47083245bfb4c03332643bd6/vision/experiments/evaluation/vloom/common/run_cron_evals_multi_task_cluster.slurm
 # Accelerate will detect MASTER_ADDR/PORT automatically if they exist;
 # we keep them for reproducibility.
-export MASTER_ADDR=$(scontrol show hostnames $SLURM_NODELIST | head -n 1)
-export MASTER_PORT=29500
+export MASTER_ADDR=`scontrol show hostnames $SLURM_JOB_NODELIST | head -n 1`
+# From https://i.hsfzxjy.site/2021-03-10-obtain-a-random-unused-tcp-port-with-bash/
+function unused_port() {
+    N=${1:-1}
+    comm -23 \
+        <(seq "1025" "65535" | sort) \
+        <(ss -Htan |
+            awk '{print $4}' |
+            cut -d':' -f2 |
+            sort -u) |
+        shuf |
+        head -n "$N"
+}
+
+export MASTER_PORT=$(unused_port)
+
+export NUM_PROCESSES=$(( SLURM_NNODES * SLURM_GPUS_ON_NODE ))
+
+export RDZV_CONF="rdzv_backend=c10d,rdzv_endpoint=$MASTER_ADDR:$MASTER_PORT"
+
+# note that the machine_rank value is escaped so it will be interpreted on the node actually doing the compute
+
+export LAUNCHER="accelerate launch \
+--config_file mpi_config.json \
+--num_processes $SLURM_NNODES \
+--num_machines $SLURM_NNODES \
+--rdzv_conf $RDZV_CONF \
+--machine_rank \$SLURM_PROCID \
+--main_process_ip $MASTER_ADDR \
+--main_process_port $MASTER_PORT \
+"
+
+export SCRIPT="train_llm.py"
+
+export ARGS="--max_records 0                \
+--output_dir ~/results/$SLURM_JOB_ID     \
+--num_train_epochs 3 \
+--eval_steps 1000 \
+--eval_limit 30 \
+--max_records 0 \
+--train_file ~/data/pubchem/arrow/cluster_6M_train.arrow \
+--eval_file ~/data/pubchem/arrow/cluster_6M_eval.arrow \
+"
+
+export CMD="$LAUNCHER $SCRIPT $ARGS"
+
+echo $CMD
 
 # --- Launch training ------------------------------------------------
-srun --mpi=pmix \
-     accelerate launch                       \
-        --config_file mpi_config.json \
-        --num_processes   $SLURM_NTASKS      \
-        --num_machines    $SLURM_NNODES      \
-        --machine_rank    $SLURM_PROCID      \
-        --main_process_ip    $MASTER_ADDR      \
-        --main_process_ip    $MASTER_PORT      \
-        train_llm.py                          \
-            --max_records 0                \
-            --output_dir ~/results/$SLURM_JOB_ID     \
-            --num_train_epochs 3 \
-            --eval_steps 1000 \
-            --eval_limit 30 \
-            --max_records 0 \
-            --train_file ~/data/pubchem/arrow/cluster_6M_train.arrow \
-            --eval_file ~/data/pubchem/arrow/cluster_6M_eval.arrow
+# srun --mpi=pmix $LAUNCHER $SCRIPT $ARGS
+# ibrun $CMD
+SRUN_ARGS=" \
+    --wait=60 \
+    --kill-on-bad-exit=1 \
+    "
+
+srun $SRUN_ARGS bash -lc "$CMD"
+
 #            --deepspeed ds_config.json              # if you enabled Deepspeed
