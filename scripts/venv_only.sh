782e4e58dbacf12fca6761f4c6bfbb7c8cfbbe25
Lewis Geer <lewis.geer@gmail.com>
2025-06-26 11:32:26 -0400

    remove pandas

--- scripts/venv_only.sh
+++ scripts/venv_only.sh
@@ -8,7 +8,7 @@ source ~/source/l1nus/.venv/bin/activate
 
 uv pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124
 
-uv pip install accelerate datasets evaluate 'hydra-core>=1.3.2' 'imageio>=2.37.0' 'imbalanced-learn>=0.13.0' 'jsonpickle>=4.0.5' 'levenshtein>=0.27.1' 'matplotlib>=3.10.1' 'mlflow-skinny>=2.21.2' 'numba>=0.61.0' numpy 'pandas>=2.2.3' 'polars>=1.26.0' 'pyarrow>=19.0.1' pytorch-lightning 'rdkit>=2024.9.6' 'requests>=2.32.3' rich 'scikit-learn>=1.6.1' 'scipy>=1.15.2' transformers torchmetrics 'tqdm>=4.67.1' pytest
+uv pip install accelerate datasets evaluate 'hydra-core>=1.3.2' 'imageio>=2.37.0' 'imbalanced-learn>=0.13.0' 'jsonpickle>=4.0.5' 'levenshtein>=0.27.1' 'matplotlib>=3.10.1' 'mlflow-skinny>=2.21.2' 'numba>=0.61.0' numpy 'polars>=1.26.0' 'pyarrow>=19.0.1' pytorch-lightning 'rdkit>=2024.9.6' 'requests>=2.32.3' rich 'scikit-learn>=1.6.1' 'scipy>=1.15.2' transformers torchmetrics 'tqdm>=4.67.1' pytest
 
 uv pip install -e .
 
